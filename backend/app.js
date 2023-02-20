bef98ffa8aa3d43753d03aa8d66790165237fb32
Wilson Wu <wuyecheng738@gmail.com>
2023-02-19 21:42:34 -0500

    backup

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const { isProduction }= require('./config/keys')
 const csurf = require('csurf');
 require('./models/User')
+require('./models/Tweet');
 require('./config/passport');
 const passport = require('passport');
 
