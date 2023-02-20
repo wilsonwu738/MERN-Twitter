46fd6f73733d5f661a8ed9abae9f709438dcca60
Wilson Wu <wuyecheng738@gmail.com>
2023-02-19 22:21:14 -0500

    finish frontend p5

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
+import errors from './errors';
+
 
 const rootReducer = combineReducers({
+  session,
+  errors
   
 });
 
