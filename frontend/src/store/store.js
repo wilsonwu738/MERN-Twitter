a8c6fa670232bf40f80994495e9878a1a4e5ff92
Wilson Wu <wuyecheng738@gmail.com>
2023-02-19 22:36:20 -0500

    frontend done

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,14 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
 import errors from './errors';
+import tweets from './tweets';
+
 
 
 const rootReducer = combineReducers({
   session,
-  errors
+  errors,
+  tweets
   
 });
 
